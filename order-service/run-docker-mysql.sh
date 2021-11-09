c5b5092a1749462df863c6f73df66bc928fde04c
mikhail_yazykov <mikhail_yazykov@senla.eu>
2021-11-09 12:11:51 +0300

    update version mysql

--- order-service/run-docker-mysql.sh
+++ order-service/run-docker-mysql.sh
@@ -1,1 +1,1 @@
-docker run -d --rm --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=order-service mysql/mysql-server
+docker run -d --rm --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=order-service mysql:8.0
